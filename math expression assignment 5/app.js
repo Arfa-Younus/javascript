f9866bdefbfff4bf60d9c3b1d7fc924978cd410e
Arfa-Younus <arfayounus99@gmail.com>
2023-05-15 23:46:18 +0500

    assignment

---
+++ math expression assignment 5/app.js
@@ -0,0 +1,135 @@
+// //Write a program that take two numbers & add them in a new variable. Show the result in your browser.
+// let num1 = 8;
+// let num2 = 6;
+// let sum = num1 + num2;
+// console.log("sum of " + num1 + " and " + num2 + " is " + sum );
+
+
+
+// //Write a program that take two numbers & subdtract them in a new variable. Show the result in your browser.
+// let num1 = 8;
+// let num2 = 6;
+// let sum = num1 - num2;
+// console.log("sum of " + num1 + " and " + num2 + " is " + sum );
+
+
+
+// //Write a program that take two numbers & multiply them in a new variable. Show the result in your browser.
+// let num1 = 8;
+// let num2 = 6;
+// let sum = num1 * num2;
+// console.log("sum of " + num1 + " and " + num2 + " is " + sum );
+
+
+
+// //Write a program that take two numbers & divide them in a new variable. Show the result in your browser.
+// let num1 = 8;
+// let num2 = 6;
+// let sum = num1 / num2;
+// console.log("sum of " + num1 + " and " + num2 + " is " + sum );
+
+
+
+// //Write a program that take two numbers & modulus them in a new variable. Show the result in your browser.
+// let num1 = 8;
+// let num2 = 6;
+// let sum = num1 % num2;
+// console.log("sum of " + num1 + " and " + num2 + " is " + sum );
+
+
+
+
+// //Do the following using JS Mathematic Expressions
+// //a. Declare a variable.
+// let var1;
+
+// //b. Show the value of variable in your browser like “Value after variable declaration is: ??”.
+// console.log("Value after variable declaration is: " +var1);
+
+// //c. Initialize the variable with some number.
+// var1 = 6;
+
+// //d. Show the value of variable in your browser like “Initial value: 5”.
+// console.log("initial value: " + var1);
+
+// //Increment the variable.
+// var1++;
+
+// //f. Show the value of variable in your browser like “Value after increment is: 6”.
+// console.log("value after increment is: " + var1);
+
+// //g. Add 7 to the variable.
+// var1 += 7;
+
+// //h. Show the value of variable in your browser like “Value after addition is: 13”.
+// console.log("value after addition is: " + var1);
+
+// // Decrement the variable.
+// var1--;
+
+// //j. Show the value of variable in your browser like “Value after decrement is: 12”.
+// console.log("value after decrement is: " + var1);
+
+// //k. Show the remainder after dividing the variable’s value by 3.
+// var1 %= 3;
+
+// //l. Output : “The remainder is : 0”.
+// console.log("The remainder is : " + var1);
+
+
+
+
+
+// //4. Cost of one movie ticket is 600 PKR. Write a script to store ticket price in a variable & calculate the cost of buying 5 tickets to a movie
+// let ticketPrice = 600;
+// let tickets = 5;
+// let totalCost = tickets * ticketPrice;
+// console.log("total cost to buy "+ tickets+ " tickets "+ "is " + totalCost+ "PKR");
+
+
+
+
+
+// //5. Write a script to display multiplication table of any number in your browser.
+// let number = 4;
+// console.log("Multiplication table of " + number);
+// multiply1 = number + " " + "x" + " "+ 1 + " "+ "=" + number * 1;
+// multiply2 = number + " " + "x" + " "+ 2 + " "+ "=" + number * 2;
+// multiply3 = number + " " + "x" + " "+ 3 + " "+ "=" + number * 3;
+// multiply4 = number + " " + "x" + " "+ 4 + " "+ "=" + number * 4;
+// multiply5 = number + " " + "x" + " "+ 5 + " "+ "=" + number * 5;
+// multiply6 = number + " " + "x" + " "+ 6 + " "+ "=" + number * 6;
+// multiply7 = number + " " + "x" + " "+ 7 + " "+ "=" + number * 7;
+// multiply8 = number + " " + "x" + " "+ 8 + " "+ "=" + number * 8;
+// multiply9 = number + " " + "x" + " "+ 9 + " "+ "=" + number * 9;
+// multiply10 = number + " " + "x" + " "+ 10 + " "+ "=" + number * 10;
+// table = multiply1 + "\n" + multiply2 + "\n" + multiply3  + "\n" + multiply4  + "\n" + multiply5  + "\n" + multiply6  + "\n" + multiply7  + "\n" + multiply8  + "\n" + multiply9  + "\n" + multiply10;
+// console.log(table);
+
+
+
+
+
+// //6. The Temperature Converter: It’s hot out! Let’s make a converter based on the steps here.
+// //. Store a Celsius temperature into a variable.
+// let celciusTemp = 24;
+
+// //b. Convert it to Fahrenheit & output “NNoC is NNoF”.
+// let fahrenheitTemp = (celciusTemp * 9 / 5) + 32;
+// console.log(fahrenheitTemp + "°F is " + celciusTemp + "°C");
+
+// //c.Now store a Fahrenheit temperature into a variable.
+// let fahrenheitTemp2 = fahrenheitTemp;
+
+// //d. Convert it to Celsius & output “NNoF is NNoC”.
+// let celciusTemp2 =(fahrenheitTemp2 - 32)* 5 / 9;
+// console.log(celciusTemp2 + "°C is " + fahrenheitTemp2 + "°F");
+
+
+
+
+
+
+
+
+
---
+++ variable (legal illegal) 4/app.js
@@ -0,0 +1,23 @@
+// //1. Declare 3 variables in one statement.
+// let var1, var2, var3;
+
+
+
+// 2. // Declare 5 legal & 5 illegal variable names.
+
+// //5 legal
+// let  Var1, var_two, VarThree, VARIABLE,_var5_;
+
+// //5 illegal
+// let --var, 9var, !$abc$, \n, const;  
+
+
+
+// //Display this in your browser
+// let display ="Rules for naming JS variables";
+// let var1= "Variable names can only contain numbers,$ and _. For example $my_1stVariable";
+// let var2= "Variables must begin with a letter, $ and _. For example $name, _name or name";
+// let var3= "Variable names are case sensitive";
+// let var4= "Variable names should not be JS keywords"
+// let var5= "<h1><strong><big>" + display + "</big></strong></h1>" + var1 +"<br>"+ var2 +"<br>"+ var3 +"<br>"+ var4;
+// document.write(var5);
