6d67fdd90aee6f02a0c6b7204632f10abd383b7a
Arfa-Younus <arfayounus99@gmail.com>
2023-05-15 15:42:40 +0500

    assignment 3 done

---
+++ variable assignment 3/app.js
@@ -0,0 +1,33 @@
+// // Declare a variable called age & assign to it your age. Show your age in an alert box.
+// let age = "I am 24 years old";
+// alert(age);
+
+
+
+
+
+// //Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: “You have visited this site N times”.
+// let visitor = "You have visited this site N times";
+// alert(visitor);
+
+
+
+
+
+// //Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
+// let birthYear =1999;
+// document.write("My Birth Year Is " + birthYear + "<br>" +"Datatype of my declared variable is number");
+
+
+
+
+
+// //A visitor visits an online clothing store www.xyzClothing.com . Write a script to store in variables the following information:
+// // a. Visitor’s name
+// // b. Product title
+// // c. Quantity i.e. how many products a visitor wants to order
+// // Show the following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+// let VisitorName = "John Doe";
+// let ProductTitle = "T-shirt";
+// let Quantity ="5";
+// document.write(VisitorName + " ordered " + Quantity + " " + ProductTitle + "(s) on XYZ Clothing store");
